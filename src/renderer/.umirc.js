6a6f16e5ac0c3c782adf35f84510134d51bdd2b6
Anane <ananej71@gmail.com>
2019-01-14 22:36:15 +0000

    new renderer

--- src/renderer/.umirc.js
+++ src/renderer/.umirc.js
@@ -1,31 +1,26 @@
+
 // ref: https://umijs.org/config/
 export default {
-  publicPath: './',
-  // runtimePublicPath: true,
-  base: '/',
-  outputPath: '../../app/dist/renderer',
   treeShaking: true,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    [
-      'umi-plugin-react',
-      {
-        antd: false,
-        dva: true,
-        dynamicImport: { webpackChunkName: true },
-        title: 'UmiElectron',
-        dll: false,
-        routes: {
-          exclude: [
-            /models\//,
-            /services\//,
-            /model\.(t|j)sx?$/,
-            /service\.(t|j)sx?$/,
-
-            /components\//,
-          ],
-        },
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'renderer',
+      dll: false,
+      routes: {
+        exclude: [
+        
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+        
+          /components\//,
+        ],
       },
-    ],
+    }],
   ],
-};
+}
--- src/renderer2/src/pages/index.tsx
+++ src/renderer2/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function() {
     <div className={styles.normal}>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>To get started, edit <code>src/renderer/pages/index.tsx</code> and save to reload.</li>
         <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
       </ul>
     </div>
